55e087b0fcec7d752168c15b76dcb64cfb8d7b0e
PraveenSara <praveensaravana558@gmail.com>
2025-12-22 20:13:25 +0530

    Added day 5 tasks.

---
+++ day5_django_api/users/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def test_api(request):
+    return Response({
+        "message": "Django REST API is working!",
+        "status": "success"
+    })
+
