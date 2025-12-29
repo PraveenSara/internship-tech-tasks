058c0c9e2e524d23084b6cfb009efc612f127530
PraveenSara <praveensaravana558@gmail.com>
2025-12-29 09:44:07 +0530

    Day 11 : Rate limiting using Throttle

--- day5_django_api/backend/settings.py
+++ day5_django_api/backend/settings.py
@@ -47,6 +47,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '5/min',
+    },
 }
 
 MIDDLEWARE = [
